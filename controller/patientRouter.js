c90eb00fabd755266384e16c20bc011a3c66d577
aswathyappukuttan2001 <aswathyappukuttan2019>
2024-02-08 15:30:17 +0530

    patiententry updated

---
+++ controller/patientRouter.js
@@ -0,0 +1,23 @@
+const express=require("express")
+const patientModel=require("../models/patientModel")
+
+const router=express.Router()
+
+router.post("/add",async(req,res)=>{
+    let data=req.body
+    let patient=new patientModel(data)
+    let result=await patient.save()
+    res.json(
+        {
+            status:"success"
+        }
+    )
+})
+
+router.get("/view",async(req,res)=>{
+    let data=await patientModel.find()
+    res.json(data)
+   
+})
+
+module.exports=router
